ac61b7d1dcd4c29c3312f21d9cb4aebf9393e7e4
juand1809 <jd.velasquezf@uniandes.edu.co>
2019-03-28 12:44:40 +0000

    complejo.py

--- complejo.py
+++ complejo.py
@@ -1,3 +1,21 @@
 import numpy as np
+import math
+
+class Complejo ():
+    def __init__ (self,real,imaginario):
+        self.real = real
+        self.imaginario = imaginario
+        
+    def conjugado(self):
+        self.imaginario = -self.imaginario
+    
+    def calcula_norma(self):
+        self.norma = np.sqrt((self.real ** 2) + (self.imaginario **2))
+    
+    def pow(self,n):
+        self.pow = math.pow(complex(self.real,self.imaginario),n)
+    
+
+        
 
 
